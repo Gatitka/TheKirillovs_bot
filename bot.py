09d3912c3d373700bdde0a87ec4d822611b42b89
Nataly Kirillova <gatitka@yandex.ru>
2023-09-09 18:28:11 +0200

    address to server DB added

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from db import BotDB
 from dispatcher import dp
 
 BotDB = BotDB('accountant.db')
+# BotDB = BotDB('/home/Admin/TheKirillovs_bot/accountant.db')
+# connection to DB on distant server
 
 if __name__ == "__main__":
     aiogram.executor.start_polling(dp, skip_updates=True)
